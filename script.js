93dd9a8f45e2a174a8e6a6196006b9e844028837
guno <guno.mail@gmail.com>
2019-05-27 00:42:23 +0200

    added jquery

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function toCelsius(fahrenheit) {
     return (5/9) * (fahrenheit-32);
   }
   document.getElementById("demo").innerHTML = toCelsius(100);
+
+  $(document).ready(function(){
+    $('#yellow').click(function () { 
+        $('body').css("background", "yellow");
+        
+    });
+
+    $('#white').click(function () { 
+        $('body').css("background", "white");
+        
+    });
+
+    $('#default').click(function () { 
+      $('body').css("background", "chartreuse");
+      
+  });
+
+});
