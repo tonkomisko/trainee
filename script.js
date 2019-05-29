4f460031bad0924e1f636f5b3bd3c2c4efed8f8d
guno <guno.mail@gmail.com>
2019-05-29 14:04:47 +0200

    news

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 function toCelsius(fahrenheit) {
-    return (5/9) * (fahrenheit-32);
-  }
-  document.getElementById("demo").innerHTML = toCelsius(100);
+  return (5 / 9) * (fahrenheit - 32);
+}
+document.getElementById("demo").innerHTML = toCelsius(100);
 
-  $(document).ready(function(){
-    $('#yellow').click(function () { 
-        $('body').css("background", "yellow");
-        
-    });
+$(document).ready(function () {
+  $('#yellow').click(function () {
+    $('body').css("background", "yellow");
+    debugger;
+    // $(this).css("background", "yellow").css("color", "red");
+    $(this).css({"background": "yellow", "color": "red", "width": });
 
-    $('#white').click(function () { 
-        $('body').css("background", "white");
-        
-    });
+  });
 
-    $('#default').click(function () { 
-      $('body').css("background", "chartreuse");
-      
+  $('#white').click(function () {
+    $('body').css("background", "white");
+
+  });
+
+  $('#default').click(function () {
+    $('body').css("background", "chartreuse");
+
   });
 
 });
