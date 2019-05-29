a4dc03e0e08b3629dfd970c4c0f4b7cd6e5e0bf9
Anton Suslik <tonkomisko@gmail.com>
2019-05-29 13:59:56 +0200

    jquery updated funcions and stuff

--- myscripts.js
+++ myscripts.js
@@ -1,23 +1,42 @@
 
 function toCelsius(f) {
- debugger;
-  return (5/9) * (f-32);
-}
-document.getElementById("demo").innerHTML = toCelsius(77);
-
-function myCoolFunction() {
-// debugger;
-  document.getElementById("test_2").innerHTML = "I've just been modified.";
-  document.getElementById("test_2").style.color = "orange";
-  document.getElementById("test_2").style.background = "white";
+  //  debugger;
+  return (5 / 9) * (f - 32);
 }
 
-// $(document).ready(function(){
-  // $.toCelsius(f) = function(){
-    // debugger;
-    //  return (5/9) * (f-32);
-  //  }
-  // $("#demo").html(this.toCelsius(77));
+function getWidth(a) {
+  debugger;
+  console.log(a);
+  var aval = $(a).css("width");
+  return aval;
 
+}
+
+// document.getElementById("demo").innerHTML = toCelsius(77);
+
+// function myCoolFunction() {
+// // debugger;
+//   document.getElementById("test_2").innerHTML = "I've just been modified.";
+//   document.getElementById("test_2").style.color = "orange";
+//   document.getElementById("test_2").style.background = "white";
 // }
-// )
+//jquery rewrite 
+$(document).ready(function () {
+  $("button#test_2").click(function () {
+    $(this).html("I've just been modified via jQuery.")
+      .css({ "color": "orange", "background": "white", "width": "200px" });
+ 
+      getWidth(this);
+
+      // $(this).css({"color": "orange", "background": "white", "width": "200px"});
+  });
+});
+
+$(document).ready(function () {
+  // debugger;
+  $("#demo").click(function () {
+    // debugger;
+    $(this).html(toCelsius(77));
+
+  });
+});
