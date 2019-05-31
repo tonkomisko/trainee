d7e3c376ab761cea106fa2f14d2c26440ec7c61b
guno <guno.mail@gmail.com>
2019-05-31 22:37:34 +0200

    news

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function toCelsius(fahrenheit) {
-  return (5 / 9) * (fahrenheit - 32);
-}
-document.getElementById("demo").innerHTML = toCelsius(100);
+// function toCelsius(fahrenheit) {
+//   return (5 / 9) * (fahrenheit - 32);
+// }
+// document.getElementById("demo").innerHTML = toCelsius(100);
 
 function getWidth(a) {
   return $(a).css("width");
