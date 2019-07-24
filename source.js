1a4ac4e96522577d3bb89af696cb47117d871516
guno <guno.mail@gmail.com>
2019-07-24 12:59:44 +0200

    json

--- source.js
+++ source.js
@@ -1,10 +1,26 @@
+// import {$,jQuery} from 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js';
+
 /** 
  Initial data definition into todo list
  @var {tableData}
 */
 
 export const tableData = [
-    { id: 1, title: 'John Jacob Astor', action: 'Delete', actionTwo: 'Edit' },
-    { id: '5', title: 'Mary-a', action: 'Delete', actionTwo: 'Edit' },
-    { id: '02', title: 'July Augustine', action: 'Delete', actionTwo: 'Edit' }
+    // { id: 1, title: 'John Jacob Astor', action: 'Delete', actionTwo: 'Edit' },
+    // { id: '5', title: 'Mary-a', action: 'Delete', actionTwo: 'Edit' },
+    // { id: '02', title: 'July Augustine', action: 'Delete', actionTwo: 'Edit' }
 ];
+
+// export var jqxhr = $.getJSON( "data.json", function() {
+//     console.log( "success" );
+//   })
+//     .done(function() {
+//       console.log( "second success" );
+//     })
+//     .fail(function() {
+//       console.log( "error" );
+//     })
+//     .always(function() {
+//       console.log( "complete" );
+//     });
+  
